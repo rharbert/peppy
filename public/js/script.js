5da67f24f8f58439bd0068cf6872a4fa0c43772c
Rachael <rachael@bodigiti.com>
2019-01-06 16:01:05 -0500

    Popups for articles: fixed open and close functionality for selected article

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,15 @@
 //Functions Functions Functions
 
-/* Toggle Article Contents */
-// $(".expand").on("click", function () {
-//   let contents = $(this).parent().siblings(".article-contents").html();
-//   $(".article-container").html(contents);
-// });
-
 /* Popups Open and Close */
 $(".open").on("click", function() {
 	let contents = $(this).parents(".article").find(".popup-content").html();
-	
-	// console.log("contents", contents);
 	$(this).parents(".article").find(".popup-overlay").addClass("active");
-	$(".popup-overlay").html(contents);
+	$(this).parents(".article").find(".popup-overlay").html(contents);
 });
 
 $(".popup-overlay").on("click", function() {
 	$(".popup-overlay").removeClass("active");
 });
-
-// $(".close").on("click", function() {
-// 	// $(this).parents(".article").find(".popup-overlay, .popup-content.active").removeClass("active");
-// 	// let contentsClose = $(this).parents(".article").html();
-// 	// console.log("contents", contentsClose);
-// 	alert("hello");
-// });
 
 /* Go to Top Button */
 window.onscroll = function() {scrollFunction()};
